a26655bf228b9b0aa4fa5df57c240f6d84a954d2
vedPrakashGit <91825651+vedPrakashGit@users.noreply.github.com>
2023-12-19 02:14:14 +0000

    Updated the server.js file to remove the redirection on default 8080 server page

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bookRoute = require("./routes/bookRoute");
 const ratingRoute = require("./routes/ratingRoute");
 const cartRoute = require("./routes/cartRoute");
 const purchaseRoute = require("./routes/purchaseRoute");
-const { expressCspHeader, INLINE, NONE, SELF } = require("express-csp-header");
 const cors = require("cors");
 
 const app = express();
@@ -13,13 +12,13 @@ const PORT = 8080;
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTNlMzM3Mzg4N2VhMjcwZWY2N2ZmYzUiLCJpYXQiOjE2OTg1OTQwOTIsImV4cCI6MTY5ODY4MDQ5Mn0.NnFGOU-wUGLQXflrO15Tk2Jtf2SJN5kjVj0iNnNb5Pk
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
 app.use("/api/users", userRoute);
 app.use("/api/books", bookRoute);
 app.use("/api/ratings", ratingRoute);
