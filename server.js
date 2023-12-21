f671257dedf55b8493290d97a97b3b1672cbb9fe
vedPrakashGit <91825651+vedPrakashGit@users.noreply.github.com>
2023-12-21 03:15:50 +0000

    After login, the page is redirecting again to login page, other pages are not rendering, so corrected the condition on servre.js file

--- server.js
+++ server.js
@@ -18,10 +18,11 @@ const dbConfig = require("./config/dbConfig");
 
 app.use(cors());
 app.use(express.json());
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('public'));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
-}
+app.use(express.static('./public'));
+// if(process.env.NODE_ENV === 'production'){
+//   app.use(express.static('public'));
+//   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
+// }
 app.use("/api/users", userRoute);
 app.use("/api/books", bookRoute);
 app.use("/api/ratings", ratingRoute);
